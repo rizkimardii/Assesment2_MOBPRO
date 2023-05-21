c2876a3a3c8b950aca37651555824f2bd94b845d
Muhamad Rizki Mardiansyah <rizkimardi@student.telkomuniversity.ac.id>
2023-05-21 21:00:34 +0700

    6706213145_Muhamad Rizki Mardiansyah_Assesment2

---
+++ app/src/main/java/org/d3if3145/assesment2/MainActivity.kt
@@ -0,0 +1,26 @@
+package org.d3if3145.assesment2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import org.d3if3145.assesment2.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
